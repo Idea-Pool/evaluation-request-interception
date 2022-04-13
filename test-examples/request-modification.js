d99ebbfc32f03a10a3f2f12df36c4d54e49713c7
Sandor Simon <sandor_simon@epam.com>
2022-04-13 11:01:11 +0200

    create test examples

---
+++ test-examples/request-modification.js
@@ -0,0 +1,7 @@
+describe('Request Modification', () => {
+  describe('Modified request verification', () => {
+    it('should be a GET method', () => {});
+    it('should have the modified URL', () => {});
+    it('should have an additional property', () => {});
+  });
+});
