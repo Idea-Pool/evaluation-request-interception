e12f803b8fc812484662614669969d83454731af
Sandor Simon <sandor_simon@epam.com>
2022-05-24 18:08:15 +0200

    implements pr change requests

--- .mocharc.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require("chai");
-
-global.expect = expect;
-
-module.exports = {
-  timeout: 30000,
-  require: "./puppeteer/mocha-hooks/hooks.ts",
-};
--- puppeteer/mocha-hooks/hooks.ts
+++ puppeteer/mocha-hooks/hooks.ts
@@ -4,10 +4,10 @@ import * as chaiAsPromised from "chai-as-promised";
 export const mochaHooks = (): Mocha.RootHookObject => {
   return {
     beforeAll() {
+      global.expect = chai.expect;
       chai.use(chaiAsPromised);
       chai.use(require("chai-subset"));
       chai.use(require("chai-json-schema"));
-    
     },
   };
 };
