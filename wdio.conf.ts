9478bf04b0906d7e7077b9c66af9c04da7f9ec8f
mgabor18 <56040199+mgabor18@users.noreply.github.com>
2022-06-09 18:29:45 +0200

    Update wdio.conf.ts

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -47,7 +47,7 @@ export const config = {
   // then the current working directory is where your `package.json` resides, so `wdio`
   // will be called from there.
   //
-  specs: ['./webdriverIO/tests/**/*.ts'],
+  specs: ['./WebdriverIO/tests/**/*.spec.ts'],
   suites: {
     requestValidation: ['./WebdriverIO/tests/request-validation.spec.ts'],
     responseModification: ['./WebdriverIO/tests/response-modification.spec.ts'],
