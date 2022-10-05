085008e3134d3df35f2c6910c855542823e71f9f
mgabor18 <56040199+mgabor18@users.noreply.github.com>
2022-10-05 21:36:41 +0200

    Create request-blocking.spec.ts

---
+++ .miscellaneous/test-examples/request-blocking.spec.ts
@@ -0,0 +1,5 @@
+describe('Request Blocking', () => {
+  describe('Blocking the request', () => {
+    it("should not let the response text appear in the UI's response section", () => {});
+  });
+});
