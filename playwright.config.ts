04494e515d3457ab44d0e16d843fb0fe9b60c894
Bence Banyasz <Bence_Banyasz@epam.com>
2022-10-24 17:00:53 +0200

    Aligned tab width

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,49 +1,49 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
+import type {PlaywrightTestConfig} from '@playwright/test';
+import {devices} from '@playwright/test';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
-  testDir: './playwright/tests',
+    testDir: './playwright/tests',
 
-  /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+    /* Maximum time one test can run for. */
+    timeout: 30 * 1000,
 
-  expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
-    timeout: 5000,
-  },
-
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
-    headless: true,
-    viewport: { width: 1920, height: 1080 },
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://reqres.in',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-
-      /* Project-specific settings. */
-      use: {
-        ...devices['Desktop Chrome'],
-      },
+    expect: {
+        /**
+         * Maximum time expect() should wait for the condition to be met.
+         * For example in `await expect(locator).toHaveText();`
+         */
+        timeout: 5000,
     },
-  ],
+
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: 'html',
+
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+        actionTimeout: 0,
+        headless: true,
+        viewport: {width: 1920, height: 1080},
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        baseURL: 'https://reqres.in',
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'on-first-retry',
+    },
+
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'chromium',
+
+            /* Project-specific settings. */
+            use: {
+                ...devices['Desktop Chrome'],
+            },
+        },
+    ],
 };
 export default config;
--- playwright/tests/response-validation.spec.ts
+++ playwright/tests/response-validation.spec.ts
@@ -1,55 +1,55 @@
-import { test, expect } from '@playwright/test';
-import { StatusCodes } from 'http-status-codes';
-import { partialBody } from '../data/response-body';
-import { fullBody } from '../data/users';
+import {test, expect} from '@playwright/test';
+import {StatusCodes} from 'http-status-codes';
+import {partialBody} from '../data/response-body';
+import {fullBody} from '../data/users';
 import * as selectors from '../data/selectors.json';
 import {Response} from 'playwright-core';
 import {Serializable} from 'worker_threads';
 
 test.describe('response validation', () => {
-  let response: Response;
-  let responseBody: Promise<Serializable>;
-  const MAX_RESPONSE_TIME = 1000;
+    let response: Response;
+    let responseBody: Promise<Serializable>;
+    const MAX_RESPONSE_TIME = 1000;
 
-  test.beforeEach(async ({ page }) => {
-    await page.goto('');
+    test.beforeEach(async ({page}) => {
+        await page.goto('');
 
-    [response] = await Promise.all([page.waitForResponse('**/users?page=2'), page.click(selectors.users)]);
-    responseBody = await response.json();
-  });
-
-  test('the status code should be 200', () => {
-    expect(response.status()).toBe(StatusCodes.OK);
-  });
-
-  test('should match partially', () => {
-    expect(responseBody).toEqual(expect.objectContaining(partialBody));
-  });
-
-  test('should match exactly', () => {
-    expect(responseBody).toEqual(fullBody);
-  });
-
-  test('the response duration should not be longer than 1s', async ({ page }) => {
-    const [request] = await Promise.all([page.waitForEvent('requestfinished'), page.click(selectors.users)]);
-    const requestStart = request.timing().requestStart;
-    const responseEnd = request.timing().responseEnd;
-    const totalRunTime = responseEnd - requestStart;
-    expect(totalRunTime).toBeLessThan(MAX_RESPONSE_TIME);
-  });
-
-  test('the number of responses should be 1', async ({ page }) => {
-    let urls = 0;
-    await page.on('response', (response) => {
-      response.url().includes('users?page=2') ? (urls += 1) : (urls += 0);
+        [response] = await Promise.all([page.waitForResponse('**/users?page=2'), page.click(selectors.users)]);
+        responseBody = await response.json();
     });
-    await page.click(selectors.users);
-    expect(urls).toEqual(1);
-  });
 
-  test('should appear on the UI', async ({ page }) => {
-    const text = await page.locator(selectors.uiUsersResponse).textContent();
-    const parsedUITextContent: string = JSON.parse(text);
-    expect(parsedUITextContent).toEqual(fullBody);
-  });
+    test('the status code should be 200', () => {
+        expect(response.status()).toBe(StatusCodes.OK);
+    });
+
+    test('should match partially', () => {
+        expect(responseBody).toEqual(expect.objectContaining(partialBody));
+    });
+
+    test('should match exactly', () => {
+        expect(responseBody).toEqual(fullBody);
+    });
+
+    test('the response duration should not be longer than 1s', async ({page}) => {
+        const [request] = await Promise.all([page.waitForEvent('requestfinished'), page.click(selectors.users)]);
+        const requestStart = request.timing().requestStart;
+        const responseEnd = request.timing().responseEnd;
+        const totalRunTime = responseEnd - requestStart;
+        expect(totalRunTime).toBeLessThan(MAX_RESPONSE_TIME);
+    });
+
+    test('the number of responses should be 1', async ({page}) => {
+        let urls = 0;
+        await page.on('response', (response) => {
+            response.url().includes('users?page=2') ? (urls += 1) : (urls += 0);
+        });
+        await page.click(selectors.users);
+        expect(urls).toEqual(1);
+    });
+
+    test('should appear on the UI', async ({page}) => {
+        const text = await page.locator(selectors.uiUsersResponse).textContent();
+        const parsedUITextContent: string = JSON.parse(text);
+        expect(parsedUITextContent).toEqual(fullBody);
+    });
 });
