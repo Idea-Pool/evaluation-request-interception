eb225f65ae20ae479c34a7cdefbba2d23b695677
mgabor18 <56040199+mgabor18@users.noreply.github.com>
2022-11-17 15:22:49 +0100

    fix tab width

--- playwright/data/response-body.ts
+++ playwright/data/response-body.ts
@@ -1,18 +1,18 @@
 export const partialBody = {
-    page: 2,
-    total: 12,
+  page: 2,
+  total: 12,
 };
 
 export const modifiedBody = {
-    data: {
-        avatar: 'https://epam.com',
-        email: 'ideapool@ideapool.epam',
-        first_name: 'Epam',
-        id: 13,
-        last_name: 'Systems',
-    },
-    support: {
-        text: 'Welcome to Epam!',
-        url: 'https://epam.com',
-    },
+  data: {
+    avatar: 'https://epam.com',
+    email: 'ideapool@ideapool.epam',
+    first_name: 'Epam',
+    id: 13,
+    last_name: 'Systems',
+  },
+  support: {
+    text: 'Welcome to Epam!',
+    url: 'https://epam.com',
+  },
 };
--- playwright/tests/request-validation.spec.ts
+++ playwright/tests/request-validation.spec.ts
@@ -1,21 +1,21 @@
-import {expect, test} from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import * as selectors from '../data/selectors.json';
-import {Request} from 'playwright-core';
+import { Request } from 'playwright-core';
 
 test.describe('request validation', () => {
-    let request: Request;
+  let request: Request;
 
-    test.beforeEach(async ({page}) => {
-        await page.goto('');
+  test.beforeEach(async ({ page }) => {
+    await page.goto('');
 
-        [request] = await Promise.all([page.waitForRequest('**/users?page=2'), page.click(selectors.users)]);
-    });
+    [request] = await Promise.all([page.waitForRequest('**/users?page=2'), page.click(selectors.users)]);
+  });
 
-    test('Should be a GET request method for listing users', () => {
-        expect(request.method()).toBe('GET');
-    });
+  test('Should be a GET request method for listing users', () => {
+    expect(request.method()).toBe('GET');
+  });
 
-    test('the request url should be correct', () => {
-        expect(request.url()).toEqual('https://reqres.in/api/users?page=2');
-    });
+  test('the request url should be correct', () => {
+    expect(request.url()).toEqual('https://reqres.in/api/users?page=2');
+  });
 });
